6ee1af2f25b64511f98c1473a214428313f3d7ad
labmalinephotos <63472749+labmalinephotos@users.noreply.github.com>
2020-04-10 15:31:11 -0400

    Update miops_clone.ino
    added logic for duration and max # of photos

--- miops_clone.ino
+++ miops_clone.ino
@@ -1,16 +1,29 @@
-int i = 0;
+int i = 1;
+int maxpics = 10;
+int delay = 10; //seconds
+int bulb = 0; //toggle for bulb mode
+int duration = 10;
+
 void setup() {
   pinMode(1, OUTPUT); //autofocus ONLY
   pinMode(2, OUTPUT); //focus + shutter, better to leave in manual focus though for faster pics
-  Serial.begin(9600);
+  //Serial.begin(9600); for debugging purposes 
 }
 
 void loop() 
 {
-    digitalWrite(2, LOW);
-    delay(5000);
-    digitalWrite(2, HIGH);
-    delay(50);
-    Serial.println(i);
-    i = i + 1;
+    if(i <= maxpics)
+    {
+      digitalWrite(2, LOW);
+      delay(duration*1000);
+      digitalWrite(2, HIGH);
+      if(bulb == 1)
+      {
+        delay(duration*1000);
+      }
+      else
+        delay(50); //delay for autofocus + shutter, without this it will not work reliably
+      //Serial.println(i);
+      i = i + 1;
+    }
 }
